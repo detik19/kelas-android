c091a2b1cda68bf8d1127ebd1eb9b435705bc890
detik19 <detik19@a0d1a78c-fd2e-2a6e-2b6b-63e0208a64bc>
2013-05-15 22:42:03 +0000

    tutorial 2

--- trunk/net.ruangtedy.android.latih.gmapv2/src/net/ruangtedy/android/latih/gmapv2/GMapV2Activity.java
+++ trunk/net.ruangtedy.android.latih.gmapv2/src/net/ruangtedy/android/latih/gmapv2/GMapV2Activity.java
@@ -1,15 +1,32 @@
 package net.ruangtedy.android.latih.gmapv2;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
 public class GMapV2Activity extends FragmentActivity {
 
+	private GoogleMap mGmaps;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		FragmentManager myFragmentManager=getSupportFragmentManager();
+		SupportMapFragment mySupportMapFragment= (SupportMapFragment) myFragmentManager.findFragmentById(R.id.map);
+		mGmaps=mySupportMapFragment.getMap();
+		
+		//my location
+		mGmaps.setMyLocationEnabled(true);
+	
+		//mGmaps.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		//mGmaps.setMapType(GoogleMap.MAP_TYPE_NORMAL);\
+		//mGmaps.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+		mGmaps.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 	}
 
 	@Override
